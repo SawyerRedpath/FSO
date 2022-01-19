b5a9665814a7fac4b9d8442887e21bf052252bf6
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2022-01-19 15:24:04 -0800

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Person from './Person';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewNameChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <div>new name state debug: {newName}</div>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <Person key={person.name} name={person.name} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
